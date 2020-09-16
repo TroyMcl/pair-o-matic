697fe2ad189e0ecdb662022faa86535c4aba8e86
shawn-chambers <chambers.shawn.89@gmail.com>
2020-09-16 16:01:54 -0700

    webpack install and setup

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  entry: './client/app.js',
+  output: './public/bundle.js',
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
+}
