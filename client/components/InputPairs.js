3b96d0140d51fc1ec5f34fb1b41621df185e739e
Troy <troyjmcl85@gmail.com>
2020-12-23 19:07:53 -0700

    Restructured application layout

--- client/components/InputPairs.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const InputPairs = (props) => {
-  return (
-    <div>Make pairs here!</div>
-  )
-}
-
-export default InputPairs;
--- client/components/MakePairs.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const MakePairs = (props) => {
-  return (
-    <div>Have the computer make pairs</div>
-  )
-}
-
-export default MakePairs;
