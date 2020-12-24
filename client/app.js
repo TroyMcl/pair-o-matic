cc3cdef744037aefe4a9551e878bc73e76c5c2b2
Troy <troyjmcl85@gmail.com>
2020-12-23 19:07:16 -0700

    Added react router and implemented in app.j

--- client/app.js
+++ client/app.js
@@ -1,73 +1,36 @@
 import React, { useState, useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+import { Grid } from '@material-ui/core'
 import ReactDOM from 'react-dom';
-import Modal from '@material-ui/core/Modal';
 import axios from 'axios';
+import MakePairs from './components/Pairs/MakePairs';
+import Header from './components/Header';
+import { CohortContextProvider } from './context/cohortContext';
 
-import AddStudents from './components/AddStudents';
+const App = () => {
 
-// To Do: get list of all cohort numbers with axios and use to populate select tab
-
-const App  = () => {
-
-  const [displayModal, setDisplayModal] = useState(true);
-  const [cohorts, setCohorts] = useState([]);
-  
-  const closeModal = () => {
-    setDisplayModal(false);
-  }
-
-  // const openModal = () => {
-  //   setDisplayModal(true);
-  // }
-
-  useEffect(() => {
-    // const fetch = async () => {
-    //   try {
-    //     let cohortsData = await axios.get('api/cohorts/24');
-    //     console.log(cohortsData);
-    //     setCohorts(cohortsData);
-    //   } catch(err) {
-    //     console.log('some error getting data', err)
-    //   }
-    // }
-    const fetch = () => {
-      axios.get('api/cohort/25')
-        .then(data => {
-          console.log(data.data.message);
-          setCohorts(data.data.message);
-        })
-        .catch(err => console.log(err));
-    }
-    fetch();
-  }, [])
-
-
-  const body = (
-    <div className={'modal'} style={{top: 'auto', left: 'auto'}} >
-      <h2>What would you like to do?</h2>
-      <form>
-        <button>Make Pairs</button>
-        <select>
-          <option value="one">One</option>
-          <option value="two">Two</option>
-          <option value="three">Three</option>
-        </select>
-        <button>Add New Cohort</button>
-      </form>
-    </div>
-  )
-    
   return (
-    <>
-      <h1>Pair-o-Matic!!!!!!</h1>
-      {/* <Modal 
-        open={displayModal}
-        onClose={closeModal}
-      >
-        {body}
-      </Modal> */}
-      <AddStudents />
-    </>
+    <div>
+      <Router>
+        <CohortContextProvider>
+          <Header />
+          <Switch>
+            <>
+            <Grid container>
+              <Route path="/">
+                <MakePairs />
+              </Route>
+            </Grid>
+            </>
+          </Switch>
+        </CohortContextProvider>
+      </Router>
+    </div>
   )
 }
 
