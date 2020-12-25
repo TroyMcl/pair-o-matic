c3a0c61d8d26e43c6d1917e1643243c339319dfc
Troy <troyjmcl85@gmail.com>
2020-12-25 13:59:20 -0700

    Created functions to run when a student is selected in the options menu, added delete functionality

--- client/components/Pairs/PairDisplay.js
+++ client/components/Pairs/PairDisplay.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { CohortContext } from '../../context/cohortContext';
 import DeleteIcon from '@material-ui/icons/Delete';
 import {
@@ -6,7 +6,7 @@ import {
   IconButton,
   makeStyles,
   FormControl,
-  InputLabel,
+  FormControlLabel,
   Select,
   MenuItem
 } from '@material-ui/core';
@@ -15,33 +15,54 @@ const useStyles = makeStyles(theme => ({
   container: {
     height: 45,
     background: '#dcc7aa',
-    // marginTop: 5,
     paddingLeft: 10,
     display: 'flex',
     alignItems: 'center'
   },
   formSelector: {
     margin: theme.spacing(1),
-    width: '40%'
+    width: '35%'
+  },
+  deleteButton: {
+    marginLeft: 15,
   }
 }))
 
 
 
 
-const PairDisplay = (props) => {
-  const [checked, setChecked] = useState(true);
+const PairDisplay = ({index, addPairToSelectedPairs, removePair}) => {
   const classes = useStyles();
   const { cohort, roster } = useContext(CohortContext);
+
+  const [checked, setChecked] = useState(false);
+  const [studentOne, setStudentOne] = useState('');
+  const [studentTwo, setStudentTwo] = useState('');
+
+  const selectStudent =(setPosition, name) => {
+    setPosition(name);
+  }
+
+  useEffect(() => {
+    addPairToSelectedPairs(studentOne, studentTwo, index)
+  },[studentOne, studentTwo])
+
+  const confirmPair = () => {
+    setChecked(!checked);
+  }
+
+  const deletePair= (index) => {
+    removePair(index)
+  }
 
   return (
     <div className={classes.container} >
       <FormControl className={classes.formSelector}>
-        {/* <InputLabel htmlFor="student1">Student</InputLabel> */}
         <Select
-          labelId="student1"
-          id="student1"
-          onChange={(e) => console.log(e.target.value)}
+          value={studentOne}
+          labelId="studentOne"
+          id="studentOne"
+          onChange={(e) => selectStudent(setStudentOne, e.target.value)}
         >
           {roster.map(item => {
             return (<MenuItem value={item} key={item}>{item}</MenuItem>)
@@ -49,25 +70,35 @@ const PairDisplay = (props) => {
         </Select>
       </FormControl>
       <FormControl className={classes.formSelector}>
-        {/* <InputLabel htmlFor="student2">Student</InputLabel> */}
         <Select
-          labelId="student2"
-          id="student2"
-          onChange={(e) => console.log(e.target.value)}
+          value={studentTwo}
+          labelId="studentTwo"
+          id="studentTwo"
+          onChange={(e) => selectStudent(setStudentTwo, e.target.value)}
         >
           {roster.map(item => {
             return (<MenuItem value={item} key={item}>{item}</MenuItem>)
           })}
         </Select>
       </FormControl>
-      <Checkbox
-        checked={checked}
-        onChange={() => console.log('checked')}
-        inputProps={{ 'aria-label': 'secondary checkbox' }}
+      <FormControlLabel
+        className={classes.checkboxLabel}
+        value={checked}
+        control={<Checkbox checked={checked} color="secondary" /> }
+        label="Confirm"
+        labelPlacement="start"
+        onClick={() => confirmPair()}
+        // <Checkbox
+        //   checked={checked}
+        //   onChange={() => console.log('checked')}
+        //   inputProps={{ 'aria-label': 'secondary checkbox' }}
+        // />
       />
       <IconButton
+        className={classes.deleteButton}
         color="secondary"
         size="medium"
+        onClick={() => deletePair(index)}
       >
         <DeleteIcon />
       </IconButton>
