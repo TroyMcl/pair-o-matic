22dcc32fbaa0993f2e383f89dda46745fd7ec4b6
Troy <troyjmcl85@gmail.com>
2020-12-27 15:31:52 -0700

    Updated routes: added /edit and /pairs route, / now renders landing page component instead of pairs component

--- client/app.js
+++ client/app.js
@@ -1,17 +1,20 @@
 import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
 } from 'react-router-dom';
+
 import { Grid } from '@material-ui/core'
-import ReactDOM from 'react-dom';
-import MakePairs from './components/Pairs/MakePairs';
-import Header from './components/Header';
-import { CohortContextProvider } from './context/cohortContext';
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from './theme';
+
+import { CohortContextProvider } from './context/cohortContext';
+import MakePairs from './components/Pairs/MakePairs';
+import Header from './components/Header';
+import LandingPage from './components/LandingPage'
 
 const App = () => {
 
@@ -26,9 +29,21 @@ const App = () => {
                 <Grid container>
                   <Grid item xs={1} />
                   <Grid item xs={10}>
-                    <Route path="/">
-                      <MakePairs />
-                    </Route>
+                    <Route
+                      path="/"
+                      exact
+                      render={(props) => <LandingPage {...props} />}
+                    />
+                    <Route
+                      path="/pairs"
+                      exact
+                      render={(props) => <MakePairs {...props}/>}
+                    />
+                    <Route
+                      path="/edit"
+                      exact
+                      render={(props) => <MakePairs {...props} />}
+                    />
                   </Grid>
                   <Grid item xs={1} />
                 </Grid>
