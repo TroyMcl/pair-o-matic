75641b007ad74d42755dadca159c780c8bae3b04
Troy <troyjmcl85@gmail.com>
2020-12-27 15:30:42 -0700

    Created landing component for user to pick from 3 options of app. User inputs cohort on landing page to update context before going to feature

---
+++ client/components/LandingPage.js
@@ -0,0 +1,91 @@
+import React, { useState, useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { CohortContext } from '../context/cohortContext';
+import {
+  Box,
+  TextField,
+  Button,
+  makeStyles,
+  Typography
+} from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  existingCohortContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    marginBottom: 15
+  },
+  inputText: {
+    fontSize: 24,
+    width: 250,
+  },
+  pairButtons: {
+    margin: 15,
+    textDecoration: 'none',
+  }
+}))
+
+
+const LandingPage = (props) => {
+  const [isError, setIsError] = useState(false);
+
+  const { fetchRoster, selectedCohort, setSelectedCohort } = useContext(CohortContext);
+  const classes = useStyles();
+
+  const updateSelectedCohort = (char) => {
+    setSelectedCohort(char);
+  }
+
+  const queryDatabase = () => {
+    fetchRoster(selectedCohort)
+  }
+
+  return (
+    <Box>
+      <Box className={classes.existingCohortContainer}>
+        <TextField
+          className={classes.inputText}
+          error={isError}
+          id="select-cohort"
+          label="Select Cohort"
+          value={selectedCohort}
+          placeholder="Select Cohort Number"
+          color='secondary'
+          onChange={(e) => updateSelectedCohort(e.target.value)}
+        />
+        <Link
+          to="/pairs"
+          className={classes.pairButtons}
+        >
+          <Button
+            onClick={() => queryDatabase()}
+            variant="contained"
+            color='secondary'
+          >
+            Generate Pairs
+          </Button>
+        </Link>
+        <Link
+            to="/edit"
+            className={classes.pairButtons}
+          >
+          <Button
+            onClick={() => queryDatabase()}
+            variant="contained"
+            color='secondary'
+          >
+            Edit Cohort or Student
+          </Button>
+        </Link>
+      </Box>
+      <Button
+        variant='contained'
+        color='primary'
+      >
+        Create New Cohort
+      </Button>
+    </Box>
+  )
+}
+
+export default LandingPage;
