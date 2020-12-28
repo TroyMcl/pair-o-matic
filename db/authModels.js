916cb7606619506cc8e863e8c5dc5ec2d6bebfbe
Troy <troyjmcl85@gmail.com>
2020-12-28 11:35:23 -0700

    created files for authorization: authModels interacts with auth docs, auth.js is for auth middleware functions

---
+++ db/authModels.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const authSchema = new mongoose.Schema({
+  userName: String,
+  email: String,
+  password: String,
+  token: String
+});
+
+const Auth = mongoose.model('Auth', authSchema);
+
+module.exports.findUserByUserName = async (userName) => {
+  try {
+    let user = await Auth.findOne({userName: userName});
+    return user;
+  } catch(err) {
+    console.log(err)
+    return err;
+  }
+}
+
+module.exports.checkUserNameTaken = async (userName) => {
+  try {
+    let userNameCount = await Auth.countDocuments({userName: userName});
+    console.log('username count', userNameCount)
+    if (userNameCount === 0) {
+      return false;
+    } else {
+      return true;
+    }
+  } catch(err) {
+    console.log(err)
+    return err;
+  }
+}
+
+module.exports.checkEmailTaken = async (email) => {
+  try {
+    let emailCount = await Auth.countDocuments({email: email});
+    console.log('email count', emailCount)
+    if (emailCount === 0) {
+      return false;
+    } else {
+      return true;
+    }
+  } catch(err) {
+    console.log(err)
+    return err;
+  }
+}
+
+module.exports.updateUser = async (user) => {
+  try {
+    await Auth.findOneAndUpdate({email: user.email}, user, {upsert: true});
+    return true
+  } catch(err) {
+    return err
+  }
+}
---
+++ server/auth.js
@@ -0,0 +1,105 @@
+const {
+  findUserByUserName,
+  checkEmailTaken,
+  checkUserNameTaken,
+  updateUser
+} = require('../db/authModels');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const generateToken = (userName) => {
+  const token = jwt.sign({
+    userName
+  },
+  'changeMeToEnvVar',
+  {
+    expiresIn: "30 days"
+  })
+  return token;
+}
+
+module.exports.login = async (req, res, next) => {
+  const { userName, email, password } = req.body;
+  try {
+  const user = await findUserByUserName(userName);
+  if (!user ) {
+    throw 'No user'
+  }
+  const authorized = await bcrypt.compare(password, user.password);
+
+  if (authorized) {
+    const token = generateToken(userName)
+    await updateUser({email, token})
+    console.log('you are logged in had have a JWT')
+    res.cookie("bearer", token);
+    res.status(200).send({
+      status: 'success',
+      message: 'You are logged in'
+    })
+  } else {
+    throw "incorrect password"
+  }
+  } catch(err) {
+    console.log(err);
+    res.status(403).send({
+      status: 'failure',
+      message: 'You are not authorized'
+    })
+  }
+}
+
+module.exports.signup = async (req, res, next) => {
+  const { userName, email, password } = req.body;
+  try {
+    const userNameTaken = await checkUserNameTaken(userName);
+    const emailTaken = await checkEmailTaken(email);
+
+    if (!userNameTaken && !emailTaken) {
+      let hash = await bcrypt.hash(password, 14)
+      let saved = await updateUser({userName: userName, email: email, password: hash})
+      if (saved === true) {
+        res.status(200).send({
+          status: 'success',
+          message: 'New user created'
+        });
+      }
+    } else {
+      res.status(403).send({
+        status: 'failure',
+        message: 'Cannot create new account with selected user name and password'
+      })
+    }
+
+  } catch (err) {
+    res.status(500).send({
+      status: 'failure',
+      message: error
+    })
+  }
+}
+
+module.exports.verify = async (req, res, next) => {
+  const token = req.cookies.bearer;
+  console.log(req.cookies, " & ", token)
+  try {
+    const decoded = jwt.verify(token, 'changeMeToEnvVar');
+    const user = await findUserByUserName(decoded.userName);
+    if(user && user.token === token) {
+      next();
+    } else {
+      throw 'Validation Failed';
+    }
+  } catch(err) {
+    console.log(err);
+    res.status(403).send();
+  }
+  // jwt.verify(token, 'changeMeToEnvVar', (err, decoded) => {
+  //   if (err) {
+  //     console.log('not autorized no token');
+  //     res.status(403).send();
+  //   } else {
+  //     console.log('it worked')
+  //     next();
+  //   }
+  // })
+};
