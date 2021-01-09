fea71a8de8bfe460aef3ebdf5c61813bcaf2a23b
Troy <troyjmcl85@gmail.com>
2021-01-08 20:08:15 -0700

    Updated directions on landing page to make starting workflow easer to understand

--- client/components/LandingPage.js
+++ client/components/LandingPage.js
@@ -35,10 +35,10 @@ const LandingPage = (props) => {
     <Box style={{marginLeft: 240}}>
         <CohortSelector />
         <Typography paragraph className={classes.paragraphStyles}>
-          Please enter a cohort number first if you wish to generate pairs, edit a student or a cohort click the ROSTER button, then select the appropriate option on the left.
+          Please enter a cohort number and click the ROSTER button if you wish to generate pairs, edit a student or a cohort. Then select the appropriate option on the left.
         </Typography>
         <Typography paragraph className={classes.paragraphStyles}>
-          If you wish to create a new cohort just click on Create New Cohort on the left, no need to enter cohort number here.
+          If you wish to create a new cohort just click on Create New Cohort on the left, no need to enter a cohort number here.
         </Typography>
     </Box>
   )
